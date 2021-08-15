fbf9e8f5526f4631df41d3a270acb8f81cfe8b87
Lucas <lucas.humbert.28@gmail.com>
2021-08-15 20:26:33 +0100

    first commit

---
+++ src/Controller/PropertyController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
+use ContainerFf1NPRb\PaginatorInterface_82dac15;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectManager;
+use phpDocumentor\Reflection\DocBlock\Tags\PropertyRead;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class PropertyController extends AbstractController{
+
+    private $repository;
+
+    public function __construct(PropertyRepository $repository, EntityManagerInterface $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
+
+    public function index(PaginatorInterface $paginator, Request $request): Response{
+        /*
+        $property = new Property;
+        $property->setTitle('Mon premier bien')
+                ->setPrice(200000)
+                ->setRooms(4)
+                ->setBedrooms(3)
+                ->setDescription('Une petite description')
+                ->setSurface(60)
+                ->setFloor(4)
+                ->setHeat(1)
+                ->setCity('Grenoble')
+                ->setAddress('15 rue du pont')
+                ->setPostalCode(38000);
+        $em = $this->getDoctrine()->getManager();
+        $em -> persist($property);
+        $em -> flush();
+        */
+        /*
+        $repository = $this->getDoctrine()->getRepository(Property::class);
+        dump($repository);
+        */
+        /*
+        $property = $this->repository->findAllVisible();
+        dump($property);
+        */
+        /*
+        $property[0]->setSold(true);
+        $this->em->flush();
+        */
+        $properties = $paginator->paginate(
+            $this->repository->findAllVisibleQuery(),
+            $request->query->getInt('page', 1),
+            12
+        );
+        return $this->render('property/index.html.twig', [
+            'current_menu' => 'properties',
+            'properties' => $properties
+        ]);
+    }
+
+
+    public function show(string $slug, $id): Response{
+
+        $property = $this->repository->find($id);
+
+        if($property->getSlug() !== $slug){
+            return $this->redirectToRoute('property.show', [
+                'id' => $property->getId(),
+                'slug' => $property->getSlug()
+            ], 301);
+        }
+
+        return $this->render('property/show.html.twig', [
+            'property' => $property,
+            'current_menu' => 'properties'
+        ]);
+    }
+}
